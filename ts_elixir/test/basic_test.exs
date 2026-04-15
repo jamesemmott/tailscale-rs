7fcbf5c6c30d90c34e0901969b57ef8ce2cc633e
Nathan Perry <nathan@tailscale.com>
2026-04-15 09:51:08 -0400

    elixir/test: support TS_RS_TEST_NET
    Check TS_RS_TEST_NET to skip elixir tests.
    
    Signed-off-by: Nathan Perry <nathan@tailscale.com>
    Change-Id: Ia5432fac6382231f9226978cdae7f04e6a6a6964

--- ts_elixir/test/basic_test.exs
+++ ts_elixir/test/basic_test.exs
@@ -1,9 +1,15 @@
 defmodule Tailscale.Test do
   use ExUnit.Case, async: true
 
+  import Tailscale.Test.Helpers,
+    only: [check_net: 1, auth_key: 1, state_file: 1, connected_client: 1]
+
+  @net_skip !Tailscale.Test.Helpers.enable_net_tests()
+
   describe "client connect" do
     setup [:check_net, :auth_key, :state_file]
 
+    @tag skip: @net_skip
     test "connect", %{state_file: state_file, auth_key: auth_key} do
       {:ok, dev} = Tailscale.connect(state_file, auth_key)
       IO.puts("connected!")
@@ -16,29 +22,28 @@ defmodule Tailscale.Test do
   describe "connected client" do
     setup [:connected_client]
 
+    @tag skip: @net_skip
     test "ip4", %{ts: dev} do
       {:ok, ip} = Tailscale.ipv4_addr(dev)
       assert :inet.is_ipv4_address(ip)
     end
 
+    @tag skip: @net_skip
     test "ip6", %{ts: dev} do
       {:ok, ip} = Tailscale.ipv6_addr(dev)
       assert :inet.is_ipv6_address(ip)
     end
 
+    @tag skip: @net_skip
     test "udp bind", %{ts: dev} do
       {:ok, ip} = Tailscale.ipv4_addr(dev)
       {:ok, _sock} = Tailscale.Udp.bind(dev, ip, 1234)
     end
 
+    @tag skip: @net_skip
     test "tcp listen", %{ts: dev} do
       {:ok, ip} = Tailscale.ipv4_addr(dev)
       {:ok, _sock} = Tailscale.Tcp.listen(dev, ip, 1234)
     end
   end
-
-  defp check_net(ctx), do: Tailscale.Test.Helpers.check_net(ctx)
-  defp auth_key(ctx), do: Tailscale.Test.Helpers.auth_key(ctx)
-  defp state_file(ctx), do: Tailscale.Test.Helpers.state_file(ctx)
-  defp connected_client(ctx), do: Tailscale.Test.Helpers.connected_client(ctx)
 end
--- ts_elixir/test/util/helper.ex
+++ ts_elixir/test/util/helper.ex
@@ -1,5 +1,14 @@
 defmodule Tailscale.Test.Helpers do
-  @auth_env_var "TS_AUTHKEY"
+  @moduledoc """
+  Common Tailscale test helper functions.
+  """
+
+  @auth_env_var "TS_RS_TEST_AUTHKEY"
+  @net_enable_env_var "TS_RS_TEST_NET"
+
+  def enable_net_tests() do
+    truthy?(System.get_env(@net_enable_env_var))
+  end
 
   def check_net(_ctx) do
     case :gen_tcp.connect(~c"controlplane.tailscale.com", 443, [:binary, active: false]) do
@@ -14,7 +23,7 @@ defmodule Tailscale.Test.Helpers do
   def auth_key(_ctx) do
     k = System.get_env(@auth_env_var)
 
-    if k == nil || k == "" do
+    if empty?(k) do
       {:error, "#{@auth_env_var} not set"}
     else
       {:ok, auth_key: k}
@@ -35,12 +44,27 @@ defmodule Tailscale.Test.Helpers do
     {:ok, auth_key: auth_key} = auth_key(ctx)
     {:ok, state_file: state_file} = state_file(ctx)
 
-    with {:ok, dev} <- Tailscale.connect(state_file, auth_key) do
-      # wait for ipv4 to be available (successful control connection)
-      {:ok, _ip} = Tailscale.ipv4_addr(dev)
-      {:ok, ts: dev, state_file: state_file, auth_key: auth_key}
-    else
-      _ -> {:error, "failed to start tailscale client"}
+    case Tailscale.connect(state_file, auth_key) do
+      {:ok, dev} ->
+        # wait for ipv4 to be available (successful control connection)
+        {:ok, _ip} = Tailscale.ipv4_addr(dev)
+        {:ok, ts: dev, state_file: state_file, auth_key: auth_key}
+
+      _ ->
+        {:error, "failed to start tailscale client"}
     end
   end
+
+  defp empty?(nil), do: true
+  defp empty?(""), do: true
+  defp empty?(_), do: false
+
+  defp truthy?(nil), do: false
+  defp truthy?(x), do: x |> String.downcase() |> _truthy?
+
+  defp _truthy?("1"), do: true
+  defp _truthy?("true"), do: true
+  defp _truthy?("yes"), do: true
+  defp _truthy?("on"), do: true
+  defp _truthy?(_), do: false
 end
